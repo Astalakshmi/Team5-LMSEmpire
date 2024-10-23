d4ce933c54021d8759af4fa867746306ecfd6641
Astalakshmi <asta.dheek@gmail.com>
2024-10-22 23:54:32 -0700

    Base Structure of BDD

---
+++ src/test/java/hooks/HooksBase.java
@@ -0,0 +1,43 @@
+package hooks;
+
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import driverManager.WebdriverManager;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utilities.ConfigFileReader;
+
+
+public class HooksBase {
+	ConfigFileReader configFileReader = new ConfigFileReader();
+
+	
+	@Before
+	public void launchBrowser() {
+		
+		String browserType = ConfigFileReader.getBrowserType();
+		WebDriver driver = WebdriverManager.initializeDriver(browserType);
+			driver.get(configFileReader.getIndexUrl());
+		}
+	 
+	 @AfterStep
+	 public void getScreenshot(Scenario scenario) {
+		 if(scenario.isFailed()) {
+	            final byte[] screenshot = ((TakesScreenshot) WebdriverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
+	            scenario.attach(screenshot, "image/png", scenario.getName());   
+	        }  
+	 }
+
+	@After
+	public void quitbrowser(Scenario scenario) {
+
+		WebdriverManager.closeDriver();
+
+	}
+	
+}
---
+++ src/test/java/utilities/ConfigFileReader.java
@@ -0,0 +1,96 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader {
+	private Properties properties;
+
+	private final String propertyFilePath = "src//test//resources//config//Configuration.properties";
+
+	public ConfigFileReader() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(propertyFilePath));
+			properties = new Properties();
+			try {
+				properties.load(reader);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+		}
+
+	}
+
+	private static String browserType = null;
+
+	public static void setBrowserType(String browser) {
+		browserType = browser;
+	}
+
+	public static String getBrowserType() {
+
+		if (browserType != null)
+			return browserType;
+		else
+			throw new RuntimeException("browser not specified in the testng.xml");
+
+	}
+
+
+	public String getBrowser() {
+		String browser = properties.getProperty("browser");
+		if (browser != null)
+			return browser;
+		else
+			throw new RuntimeException("browser not specified in the Configuration.properties file.");
+	}
+
+	public String getPageTitle(String pageName) {
+		String title = properties.getProperty(pageName);
+		if (title != null)
+			return title;
+		else
+			throw new RuntimeException(pageName + " url not specified in the Configuration.properties file.");
+	}
+
+	public String getIndexUrl() {
+		String indexUrl = properties.getProperty("indexUrl");
+		if (indexUrl != null)
+			return indexUrl;
+		else
+			throw new RuntimeException(" Index url not specified in the Configuration.properties file.");
+	}
+
+	public String getHomeUrl() {
+		String homepageUrl = properties.getProperty("homepageUrl");
+		if (homepageUrl != null)
+			return homepageUrl;
+		else
+			throw new RuntimeException(" homepage url not specified in the Configuration.properties file.");
+	}
+	
+	public String getAlert(String alertNames) {
+		String alertName = properties.getProperty(alertNames);
+		if (alertName != null)
+			return alertName;
+		else
+			throw new RuntimeException(alertName + " alert does not match in the Configuration.properties file.");
+	}
+
+	public String getFunctionalityMessage(String functionalityName) {
+		String funcName = properties.getProperty(functionalityName);
+		if (funcName != null)
+			return funcName;
+		else
+			throw new RuntimeException(funcName + " does not match in the Configuration.properties file.");
+	}
+
+}
