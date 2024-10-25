18f22385f41a4d15306484353891dd45fa7738ee
resmy <resmyrose@gmail.com>
2024-10-25 12:59:39 -0700

    pushing to branch resmy

--- src/test/java/pageObjects/ProgramModule.java
+++ src/test/java/pageObjects/ProgramModule.java
@@ -1,5 +1,16 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ProgramModule {
+	WebDriver driver;
+	
+	@FindBy(xpath = "//button[@class='p-button-danger p-button p-component p-button-icon-only']")
+	private WebElement deleteAll;
+	@FindBy(xpath = "//input[@id='filterGlobal']")
+	private WebElement searchBtn;
+	
 
 }
